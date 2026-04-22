a039ff445bab826d39c2d043635a412b299fc480
MaxMini64 <unkindrelic64@gmail.com>
2026-04-22 13:55:58 -0600

    Agregar configuración CORS para permitir solicitudes desde dominios específicos

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.api import api_router
 
 app = FastAPI(
     title="AgroRiego API",
     description="API para gestión y monitoreo inteligente de riego agrícola.",
     version="1.0.0"
+)
+
+# Configuración CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173",
+                   "https://francisco-huerta-trujillo.github.io"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(api_router, prefix="/api/v1")
